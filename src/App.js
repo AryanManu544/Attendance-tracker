a7ea066e5d342b0aa0c99c965540be82688b81ef
Aryan Manu <aryanmanu544@gmail.com>
2025-02-19 11:24:13 +0530

    timetable deployed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Signup from "./Components/Signup";
 import PrivateRoute from "./Privateroute";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Analytics } from "@vercel/analytics/react";
 import Timetable from './Components/Timetable';
 
